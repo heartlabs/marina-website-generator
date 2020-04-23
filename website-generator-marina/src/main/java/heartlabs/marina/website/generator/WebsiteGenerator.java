400292e564d0dd122732dde420980e2a8aa5936b
heartlabs <neidhart.orlich@gmail.com>
2020-04-23 11:29:33 +0200

    Add files via upload

--- website-generator-marina/src/main/java/heartlabs/marina/website/generator/WebsiteGenerator.java
+++ website-generator-marina/src/main/java/heartlabs/marina/website/generator/WebsiteGenerator.java
@@ -3,8 +3,10 @@ package heartlabs.marina.website.generator;
 import java.io.File;
 import java.io.IOException;
 import java.io.UncheckedIOException;
+import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
@@ -12,10 +14,14 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class WebsiteGenerator {
-	public static void main(String[] args) {
+	private static final String THUMBNAIL_JPG = "thumbnail.jpg";
+	private static List<String> pageNames = Arrays.asList("index", "weddings", "lovestory", "contacts");
+	
+	public static void main(String[] args) throws IOException {
 		File templateFolder = new File("src/main/resources/heartlabs/marina/website/templates");
+		File resourcesFolder = new File("src/main/resources/heartlabs/marina/website/resources");
 		File targetFolder = new File("output");
-		File imageFolder = new File("images");
+		File sourceImageFolder = new File("images");
 		targetFolder.mkdirs();
 		
 		System.out.println(templateFolder.getAbsolutePath());
@@ -25,54 +31,98 @@ public class WebsiteGenerator {
 		List<Page> pages = new ArrayList<>();
 		dataModel.pages = pages;
 		
-		sortedChildren(templateFolder)
-			.forEach(templateFile -> { //
-				
-			String templateName = templateFile.getName();
-			if (templateName.endsWith(".ftlh")) {
-				String templateBaseName = templateName.replaceAll("\\.ftlh$", "");
-				String targetFileName = templateBaseName + ".html";
-				
-				Page page = new Page();
-				pages.add(page);
-				
-				page.templateName = templateName;
-				page.targetFile = new File(targetFolder, targetFileName);
-				page.images = new ArrayList<>();
-				page.baseName = templateBaseName;
-				page.title = Character.toUpperCase(templateBaseName.charAt(0)) + templateBaseName.substring(1);
-				page.title = page.title.replaceAll("-", " ");
-				
-				String imageSubfolderPath = "images/" + templateBaseName + "/";
-				File pageImageFolder = new File(imageFolder, templateBaseName);
-				if (pageImageFolder.exists()) {
-					sortedChildren(pageImageFolder) //
-						.forEach(file -> {
-							page.images.add(imageSubfolderPath + file.getName());
-							
-							File targetImageFolder = new File(targetFolder, imageSubfolderPath);
-							targetImageFolder.mkdirs();
-							
-							Path imageTargetPath = new File(targetImageFolder, file.getName()).toPath();
-							try {
-								Files.copy(file.toPath(), imageTargetPath);
-							} catch (IOException e) {
-								throw new UncheckedIOException(e);
-							}
-						});
-				}
-			}
-		});
+		pageNames.stream()
+			.map(n -> new File(templateFolder, n + ".ftlh"))
+			.filter(File::exists)
+			.forEach(templateFile -> processRootTemplate(targetFolder, sourceImageFolder, pages, templateFile));
+		
+		for (File resFile : resourcesFolder.listFiles()) {
+			Files.copy(resFile.toPath(), new File(targetFolder, resFile.getName()).toPath(), StandardCopyOption.REPLACE_EXISTING);
+		}
 		
 		for (Page page: pages) {
+			System.out.println("Rendering page " + page.title);
 			dataModel.page = page;
 			freemarkerRenderer.writeFileFromTemplate(page.templateName, dataModel, page.targetFile);
 		}
+		
+		System.out.println("Done.");
+	}
+
+	private static void processRootTemplate(File targetFolder, File sourceImageFolder, List<Page> pages, File templateFile) {
+		String templateName = templateFile.getName();
+		
+		Page page = new Page(targetFolder, templateName);
+		pages.add(page);
+
+		String templateBaseName = page.baseName;
+		
+		System.out.println("Preparing page " + templateBaseName );
+		if (templateName.equals("index.ftlh")) {
+			page.hidden = true;
+			page.title = null;
+		}
+		
+		File pageSourceImageFolder = new File(sourceImageFolder, templateBaseName);
+		if (pageSourceImageFolder.exists()) {
+			File pageTargetImageFolder = new File(targetFolder, page.baseName);
+			pageTargetImageFolder.mkdirs();
+			
+			Arrays.stream(pageSourceImageFolder.listFiles()) // 
+				.sorted(Comparator.comparing(File::getName))
+				.forEach(file -> {
+					
+					if (file.isDirectory()) {
+						Page subPage = handleSubPage(pageTargetImageFolder, file);
+						pages.add(subPage);
+						page.subPages.add(subPage);
+					} else {
+						handlePageImage(pageTargetImageFolder, page, file);
+					}
+				});
+		}
+	}
+
+	private static Page handleSubPage(File parentTargetDir, File subPageSourceDir) {
+		String subPageName = subPageSourceDir.getName();
+		File[] images = subPageSourceDir.listFiles();
+		
+		File subPageTargetDir = new File(parentTargetDir, subPageName);
+		File thumbnailTargetFile = new File(subPageTargetDir, THUMBNAIL_JPG);
+		File thumbnailSourceFile = new File(subPageSourceDir, THUMBNAIL_JPG);
+		
+		if (!thumbnailSourceFile.exists()) {
+			thumbnailSourceFile = Arrays.stream(images) //
+				.sorted(Comparator.comparing(File::getName))
+				.findFirst()
+				.orElseThrow(() -> new IllegalStateException("No image found in folder " + subPageSourceDir.getAbsolutePath()));
+		}
+		
+		subPageTargetDir.mkdirs();
+		
+		ThumbnailGenerator.createThumbnail(thumbnailSourceFile, thumbnailTargetFile);
+		
+		Page subPage = new Page(subPageTargetDir, subPageName, "subpage.ftlh");
+		subPage.hidden = true;
+		subPage.rootDirPath = "../../";
+		
+		Arrays.stream(images) //
+			.sorted(Comparator.comparing(File::getName)) //
+			.forEach(sourceImageFile -> handlePageImage(subPageTargetDir, subPage, sourceImageFile));
+			
+		return subPage;
 	}
 	
-	private static Stream<File> sortedChildren(File parent){
-		return Arrays.stream(parent.listFiles()) // 
-				.sorted(Comparator.comparing(File::getName));
+	private static void handlePageImage(File targetImageFolder, Page page, File sourceImageFile) {
+		page.images.add(sourceImageFile.getName());
+		
+		Path imageTargetPath = new File(targetImageFolder, sourceImageFile.getName()).toPath();
+		
+		try {
+			Files.copy(sourceImageFile.toPath(), imageTargetPath, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
 	}
 	
 	public static class DataModel {
@@ -82,37 +132,16 @@ public class WebsiteGenerator {
 		public List<Page> getPages() {
 			return pages;
 		}
-		
 		public Page getCurrentPage() {
 			return page;
 		}
-
-	}
-	
-	public static class Page {
-		public String templateName;
-		public String baseName;
-		public String title;
-		public File targetFile;
-		public List<String> images;
-		public Object dataModel;
 		
-		public String getTemplateName() {
-			return templateName;
+		public String getRootDir() {
+			return page.getRootDir();
 		}
-		public String getBaseName() {
-			return baseName;
+		public String getResDir() {
+			return page.getRootDir();
 		}
-		public String getTitle() {
-			return title;
-		}
-		public File getTargetFile() {
-			return targetFile;
-		}
-		public List<String> getImages() {
-			return images;
-		}
+		
 	}
 }
-
-
